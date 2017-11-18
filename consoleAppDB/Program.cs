874988950ac2da548edce812513acf3c526f0b53
Tabook22 <nmtabook@gmail.com>
2017-11-18 21:33:36 +0400

    first commit

---
+++ consoleAppDB/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleAppDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (BloggingContext db=new BloggingContext())
+            {
+
+                    db.Blog.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                    var count = db.SaveChanges();
+                    Console.WriteLine("{0} records saved to database", count);
+
+                    Console.WriteLine();
+                    Console.WriteLine("All blogs in database:");
+                    foreach (var blog in db.Blog)
+                    {
+                        Console.WriteLine(" - {0}", blog.Url);
+                    }
+
+
+                List<Post> ps = db.Post.ToList();
+                Console.WriteLine(ps.Count());
+                foreach(var itm in ps)
+                {
+                    Console.WriteLine(itm.Title);
+                   
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
